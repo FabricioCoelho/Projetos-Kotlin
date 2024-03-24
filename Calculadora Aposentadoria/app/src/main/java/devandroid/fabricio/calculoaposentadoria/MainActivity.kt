a4f8bae3ca2e8706c742ec035b5f8b354fcbb08d
Fabricio <fabriciocoelho03082000@hotmail.com>
2024-03-24 19:04:23 -0400

    Criando as variáveis e colocando a item de lista para escolher o sexo

--- Calculadora Aposentadoria/app/src/main/java/devandroid/fabricio/calculoaposentadoria/MainActivity.kt
+++ Calculadora Aposentadoria/app/src/main/java/devandroid/fabricio/calculoaposentadoria/MainActivity.kt
@@ -1,9 +1,37 @@
 package devandroid.fabricio.calculoaposentadoria
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.ArrayAdapter
 
 class MainActivity: Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //definindo o arquivo de layout
+        setContentView(R.layout.activity_main)
+        //acessando o spinner
+        val spinnerSexo = findViewById<Spinner>(R.id.spinner_sexo)
+        //acessando a caixa de idade
+        val editTexteIdade = findViewById<EditText>(R.id.edit_text_idade)
+        //acessando o botão de calcular
+        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        //acessando o texto de resultado
+        val textViewResultado = findViewById<TextView>(R.id.text_view_resultado)
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            listOf("masculino", "feminino")
+        )
+        spinnerSexo.adapter = ArrayAdapter<String>(this, android.R.layout
+            .simple_spinner_dropdown_item,
+            listOf("masculino", "feminino"))
+        spinnerSexo.adapter = adapter
+
+
     }
 }
