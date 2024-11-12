f01b2e7a13559eae24a8c8336a1c5e4912702475
Fabricio <fabriciocoelho03082000@hotmail.com>
2024-11-12 15:41:07 -0400

    Criação da primeira e segunda tela.

---
+++ mapa_ifam/app/src/main/java/devandroid/fabricio/mapa_ifam/MainActivity_principais_locais.kt
@@ -0,0 +1,11 @@
+package devandroid.fabricio.mapa_ifam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainLocationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_principais_locais)
+    }
+}
